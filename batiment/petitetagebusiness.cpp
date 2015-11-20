c8040a9ccda892536104705dace0aa81eeecffe3
LilTsubaki <omenforce.clem@neuf.fr>
2015-11-20 13:35:21 +0100

    Fix peb

--- batiment/petitetagebusiness.cpp
+++ batiment/petitetagebusiness.cpp
@@ -7,19 +7,18 @@
 
 Mesh PetitEtageBusiness::generate() const
 {
-
         MeshBuilder mb;
         Mesh pebMesh = mb.generationEtage(this);
         QVector<std::pair<Batiment*,int>> bats;
 
-        ToitBusiness tb(base, hauteur, hauteurEtage, hMax, shrinkMax, aireMin);
-        EtageBusiness eb(base, hauteur, hauteurEtage, hMax-1, shrinkMax, aireMin);
-        Division d(base, hauteur, hauteurEtage, hMax-1, shrinkMax-1, aireMin);
+        ToitBusiness tb(base, hauteur+hauteurEtage, hauteurEtage, hMax, shrinkMax, aireMin);
+        EtageBusiness eb(base, hauteur+hauteurEtage, hauteurEtage, hMax-1, shrinkMax, aireMin);
+        Division d(base, hauteur+hauteurEtage, hauteurEtage, hMax-1, shrinkMax-1, aireMin);
         bats.append(std::make_pair(&tb, 2));
 
         if(hMax > 1)
         {
-            std::make_pair(&eb, 70);
+            bats.append(std::make_pair(&eb, 70));
             if(shrinkMax > 0 && d.getPoly1().area()>aireMin && d.getPoly2().area() > aireMin)
             {
                 bats.append(std::make_pair(&d, 10));
