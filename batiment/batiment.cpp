e958a1613edc0e6fab7ead02f036dd5f4bf49677
Drazatlam <lulu.brisard@hotmail.fr>
2015-11-20 12:09:01 +0100

    refactor getRandomBatiment

--- batiment/batiment.cpp
+++ batiment/batiment.cpp
@@ -45,18 +45,12 @@ Batiment* Batiment::getRandomBatiment(const QVector<std::pair<Batiment*,int>>& b
         somme+=bats.at(i).second;
     }
 
-    if(somme==0){
-        if(bats.size()!=0){
-            return bats.at(0).first;
-        }
-        return nullptr_t;
-    }
     float rand=MathUtils::random(0,somme);
 
     somme =0;
     for(int i=0;i<bats.size();++i){
         somme+=bats.at(i).second;
-        if(rand<somme)
+        if(rand<=somme)
             return bats.at(i).first;
     }
     return nullptr;
